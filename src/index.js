80cf49461e8ad3132f3240c480ba60b6ebb85f1c
Slava Manzhela <slavamanzh@gmail.com>
2023-01-25 17:20:56 +0300

    feat: add working solution

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+    function compareNumbers(a, b) {
+        return a - b;
+    }
     if (!matrix) {
         return [];
     } else if (matrix.length==0) {
             return [];
     } else {
-         return matrix.sort((a,b)=>{
-            return a-b;
-        });
+        return (
+            matrix.reduce((acc, cur, i) => {
+              cur.sort((a, b) => !(i & 1) ? a - b : b - a).map(e => acc.push(e));
+              return acc;
+            }, [])
+          );
     }
 }
